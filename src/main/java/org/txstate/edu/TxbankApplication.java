787b3cc3e59bc625984d65ba8e19af2406517f0f
jyotigupta329 <33521919+jyotigupta329@users.noreply.github.com>
2018-02-07 19:37:28 -0600

    Spring boot project setup

---
+++ src/main/java/org/txstate/edu/TxbankApplication.java
@@ -0,0 +1,12 @@
+package org.txstate.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxbankApplication.class, args);
+	}
+}
