ce1f13168cd62c804bf649b86363179cbc77ffba
jyoti <jyoti.gupta329@gmail.com>
2018-04-25 23:02:44 -0500

    Added Junit Test cases for users

--- src/test/java/org/txstate/edu/controller/UserControllerTest.java
+++ src/test/java/org/txstate/edu/controller/UserControllerTest.java
@@ -7,13 +7,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.txstate.edu.config.TxbankApplication;
+import org.txstate.edu.model.UserForm;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by jyoti on 4/25/18.
@@ -25,23 +30,28 @@ public class UserControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    MultiValueMap<String, String> requestHeaders = null;
+    MultiValueMap<String, String> userRequestHeaders = null;
+    MultiValueMap<String, String> adminRequestHeaders = null;
 
     @Before
     public void setUp() throws Exception {
+        userRequestHeaders = login("jyoti");
+
+    }
+
+    private MultiValueMap<String, String> login(String username) {
         String requestBody = "{\n" +
-                "\t\"username\": \"jyoti\",\n" +
+                "\t\"username\": \"" + username + "\",\n" +
                 "\t\"password\": \"password\"\n" +
                 "}";
         HttpEntity<String> entity = new HttpEntity<>(requestBody);
         HttpEntity<String> response = restTemplate.exchange("/login", HttpMethod.POST, entity, String.class);
 
-        String resultString = response.getBody();
         HttpHeaders headers = response.getHeaders();
-        requestHeaders = new LinkedMultiValueMap<>();
-        requestHeaders.put("Authorization", headers.get("Authorization"));
-        requestHeaders.add("content-type","application/json");
-
+        MultiValueMap<String, String>  userRequestHeaders = new LinkedMultiValueMap<>();
+        userRequestHeaders.put("Authorization", headers.get("Authorization"));
+        userRequestHeaders.add("content-type", "application/json");
+        return userRequestHeaders;
     }
 
     @After
@@ -56,6 +66,15 @@ public class UserControllerTest {
 
     @Test
     public void getAllUsers() throws Exception {
+        adminRequestHeaders = login("admin");
+        HttpEntity<String> entity = new HttpEntity<>(null, adminRequestHeaders);
+
+        ResponseEntity<List<UserForm>> responseEntity =
+                restTemplate.exchange("/users", HttpMethod.GET, entity, new ParameterizedTypeReference<List<UserForm>>() {
+                });
+        List<UserForm> client = responseEntity.getBody();
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertTrue(client.size() > 0);
     }
 
     @Test
